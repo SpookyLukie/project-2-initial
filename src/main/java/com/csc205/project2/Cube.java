10c67a45e4e50610b288f86c3b1e9159b7719be0
SpookyLukie <71572783+SpookyLukie@users.noreply.github.com>
2021-09-23 22:58:41 -0700

    Final submission commit/push

--- src/main/java/com/csc205/project2/Cube.java
+++ src/main/java/com/csc205/project2/Cube.java
@@ -2,6 +2,7 @@ package com.csc205.project2;
 
 public class Cube extends Sphere {
 
+
     public Cube(double length) {
         super(length);
     }
@@ -14,19 +15,12 @@ public class Cube extends Sphere {
         return radius*radius*radius;
     }
 
-//    @Override
-//    public String toString() {
-//        return "Cube [surfaceArea=" + surfaceArea() + ", volume=" + volume() + "]";
-//    }
 
     public String toString() {
-        final StringBuilder sb = new StringBuilder("Sphere {");
-        sb.append("radius=").append(surfaceArea());
+        final StringBuilder sb = new StringBuilder("Cube {");
         sb.append(", surface area=").append(surfaceArea());
         sb.append(", volume=").append(volume());
         sb.append('}');
         return sb.toString();
     }
-
-
 }
--- src/main/java/com/csc205/project2/Cylinder.java
+++ src/main/java/com/csc205/project2/Cylinder.java
@@ -4,38 +4,45 @@ public class Cylinder {
 
     double radius;
     double height;
+
     public Cylinder(double radius, double height) {
         super();
         this.radius = radius;
         this.height = height;
     }
+
     public double getRadius() {
         return radius;
     }
+
     public void setRadius(double radius) {
         this.radius = radius;
     }
+
     public double getHeight() {
         return height;
     }
+
     public void setHeight(double height) {
         this.height = height;
     }
 
     double surfaceArea() {
-        return 2*22/7*radius*(radius+height);
+        return 2 * 22 / 7 * radius * (radius + height);
     }
 
     double volume() {
-        return 22/7*radius*radius*height;
+        return 22 / 7 * radius * radius * height;
     }
+
     @Override
     public String toString() {
-        return "Cylinder [surfaceArea=" + surfaceArea() + ", volume=" + volume() + "]";
+        final StringBuilder sb = new StringBuilder("Cylinder {");
+        sb.append("height=").append(height);
+        sb.append("radius=").append(radius);
+        sb.append(", surface area=").append(surfaceArea());
+        sb.append(", volume=").append(volume());
+        sb.append('}');
+        return sb.toString();
     }
-
-
-
-
-
 }
